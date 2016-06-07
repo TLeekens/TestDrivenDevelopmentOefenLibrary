103798247ba65005e3624981bf33d88f08222fe2
TLeekens <tony.leekens@skynet.be>
2016-06-07 14:22:15 +0200

    Oefening 1.6 Mock is opgelost, stubs vervangen door mockito-mocks

--- TestDrivenDevelopmentOefenLibraryTest/WinstServiceTest.cs
+++ TestDrivenDevelopmentOefenLibraryTest/WinstServiceTest.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TestDrivenDevelopmentOefenLibrary;
+using Moq;
 
 namespace TestDrivenDevelopmentOefenLibraryTest
 {
     [TestClass]
     public class WinstServiceTest
     {
+        private Mock<IOpbrengstDAO> opbrengstMockFactory;
+        private Mock<IKostDAO> kostMockFactory;
         private IOpbrengstDAO opbrengstDAO;
         private IKostDAO kostDAO;
         private WinstService winstService;
@@ -14,14 +17,20 @@ namespace TestDrivenDevelopmentOefenLibraryTest
         [TestInitialize]
         public void Initialize()
         {
-            opbrengstDAO = new OpbrengstDAOStub();
-            kostDAO = new KostDAOStub();
+            opbrengstMockFactory = new Mock<IOpbrengstDAO>();
+            opbrengstDAO = opbrengstMockFactory.Object;
+            opbrengstMockFactory.Setup(eenOpbrengstDAO => eenOpbrengstDAO.TotaleOpbrengst()).Returns(2000.0m);
+            kostMockFactory = new Mock<IKostDAO>();
+            kostDAO = kostMockFactory.Object;
+            kostMockFactory.Setup(eenKostDAO => eenKostDAO.TotaleKost()).Returns(1000.0m);
             winstService = new WinstService(opbrengstDAO, kostDAO);
         }
         [TestMethod]
         public void PropertyWinstGeeftVerschilVanTotaleOpbrengstEnTotaleKost()
         {
             Assert.AreEqual(1000.0m, winstService.Winst);
+            opbrengstMockFactory.Verify(eenOpbrengstDAO => eenOpbrengstDAO.TotaleOpbrengst());
+            kostMockFactory.Verify(eenKostDAO => eenKostDAO.TotaleKost());
         }
     }
 }
