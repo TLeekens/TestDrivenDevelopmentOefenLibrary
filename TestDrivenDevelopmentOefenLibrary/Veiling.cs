09e001edd227ba2f5863e0b1d38addb00d71903d
TLeekens <tony.leekens@skynet.be>
2016-06-07 11:06:27 +0200

    VeilingTest is ontwikkeld, alle tests falen, Veiling is klaar om geïmplementeerd te worden

---
+++ TestDrivenDevelopmentOefenLibrary/Veiling.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDrivenDevelopmentOefenLibrary
+{
+    public class Veiling
+    {
+        public void DoeBod(decimal bedrag)
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal HoogsteBod()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TestDrivenDevelopmentOefenLibraryTest/VeilingTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestDrivenDevelopmentOefenLibrary;
+
+namespace TestDrivenDevelopmentOefenLibraryTest
+{
+    [TestClass]
+    public class VeilingTest
+    {
+        private Veiling veiling;
+        [TestInitialize]
+        public void Initialize()
+        {
+            veiling = new Veiling();
+        }
+        [TestMethod, ExpectedException(typeof(ArgumentException))]
+        public void DoeBodMetEenNegatiefBodWerptEenArgumentException()
+        {
+            veiling.DoeBod(-1.0m);
+        }
+        [TestMethod]
+        public void AlsNogGeenBodWerdIngevoerdIsHetHoogsteBodNul()
+        {
+            Assert.AreEqual(decimal.Zero, veiling.HoogsteBod());
+        }
+        [TestMethod]
+        public void NaEenEnkelBodIsHetHoogsteBodHetBedragVanDatEneBod()
+        {
+            decimal bedrag = 100.0m;
+            veiling.DoeBod(bedrag);
+            Assert.AreEqual(bedrag, veiling.HoogsteBod());
+        }
+        [TestMethod]
+        public void NaMeerdereBodenIsHetHoogsteBodHetHoogsteBedrag()
+        {
+            decimal hoogsteBedrag = 200.0m;
+            veiling.DoeBod(100.0m);
+            veiling.DoeBod(hoogsteBedrag);
+            veiling.DoeBod(150.0m);
+            Assert.AreEqual(hoogsteBedrag, veiling.HoogsteBod());
+        }
+    }
+}
