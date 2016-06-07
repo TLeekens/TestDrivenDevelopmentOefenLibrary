2d186328ba494959dd5c23409a2e9ff7c2a19d24
TLeekens <tony.leekens@skynet.be>
2016-06-07 10:11:02 +0200

    WoordTest is klaar, nu moet Woord ontwikkeld worden

---
+++ TestDrivenDevelopmentOefenLibrary/Woord.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDrivenDevelopmentOefenLibrary
+{
+    public class Woord
+    {
+        public Woord(string woord)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsPalindroom()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TestDrivenDevelopmentOefenLibraryTest/WoordTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestDrivenDevelopmentOefenLibrary;
+
+namespace TestDrivenDevelopmentOefenLibraryTest
+{
+    [TestClass]
+    public class WoordTest
+    {
+        [TestMethod, ExpectedException(typeof(ArgumentException))]
+        public void ConstructorMetLegeStringWerptArgumentException()
+        {
+            new Woord(string.Empty);
+        }
+
+        [TestMethod]
+        public void IsPalindroomMetIngevoerdPalindroomMetOnevenAantalLettersGeeftTrue()
+        {
+            Assert.IsTrue(new Woord("lepel").IsPalindroom());
+        }
+
+        [TestMethod]
+        public void IsPalindroomMetIngevoerdPalindroomMetEvenAantalLettersGeeftTrue()
+        {
+            Assert.IsTrue(new Woord("neen").IsPalindroom());
+        }
+
+        [TestMethod]
+        public void IsPalindroomMetIngevoerdNietPalindroomGeeftFalse()
+        {
+            Assert.IsFalse(new Woord("vork").IsPalindroom());
+        }
+    }
+}
