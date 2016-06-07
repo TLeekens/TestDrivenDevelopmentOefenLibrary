9e200bd694e4bae996f3d8c120d1b87a339aef5d
TLeekens <tony.leekens@skynet.be>
2016-06-07 11:48:03 +0200

    ISBNTest is klaar, alle tests falen, ISBN kan geïmplementeerd worden

---
+++ TestDrivenDevelopmentOefenLibrary/ISBN.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDrivenDevelopmentOefenLibrary
+{
+    public class ISBN
+    {
+        public ISBN(long nummer)
+        {
+            throw new NotImplementedException();
+        }
+        public override string ToString()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TestDrivenDevelopmentOefenLibraryTest/ISBNTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestDrivenDevelopmentOefenLibrary;
+
+namespace TestDrivenDevelopmentOefenLibraryTest
+{
+    [TestClass]
+    public class ISBNTest
+    {
+        [TestMethod, ExpectedException(typeof(ArgumentException))]
+        public void EenNegatiefNummerWerptArgumentException()
+        {
+            new ISBN(-9789027439642);
+        }
+        [TestMethod, ExpectedException(typeof(ArgumentException))]
+        public void EenNummerMetTwaalfCijfersWerptArgumentException()
+        {
+            new ISBN(123456789012);
+        }
+
+        [TestMethod, ExpectedException(typeof(ArgumentException))]
+        public void EenNummerMetVeertienCijfersWerptArgumentException()
+        {
+            new ISBN(12345678901234);
+        }
+
+        [TestMethod, ExpectedException(typeof(ArgumentException))]
+        public void EenNummerMetDertienCijfersMetFoutEindCijferWerptArgumentException()
+        {
+            new ISBN(9789027439643);
+        }
+
+        [TestMethod]
+        public void BijEenCorrectISBNNummerGeeftDeToStringDatzelfdeNummerAlsEenStringTerug()
+        {
+            long nummer = 9789027439642;
+            Assert.AreEqual(nummer.ToString(), new ISBN(nummer).ToString());
+        }
+
+        [TestMethod]
+        public void BijEenCorrectISBNNummerMetNulAchteraanGeeftDeToStringDatzelfdeNummerAlsEenStringTerug()
+        {
+            long nummer = 9789227439640;
+            Assert.AreEqual(nummer.ToString(), new ISBN(nummer).ToString());
+        }
+    }
+}
