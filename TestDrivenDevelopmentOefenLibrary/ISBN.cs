48f07963a7c6af445b2b2960e2589f383d30cd3c
TLeekens <tony.leekens@skynet.be>
2016-06-07 13:48:26 +0200

    ISBN is correct geïmplementeerd, alle tests slagen

--- TestDrivenDevelopmentOefenLibrary/ISBN.cs
+++ TestDrivenDevelopmentOefenLibrary/ISBN.cs
@@ -8,13 +8,45 @@ namespace TestDrivenDevelopmentOefenLibrary
 {
     public class ISBN
     {
+        private long iSBNNummer;
         public ISBN(long nummer)
         {
-            throw new NotImplementedException();
+            //controleren op negativiteit
+            if (nummer < 0)
+            throw new ArgumentException();
+            //we maken van de long een string om daar handelingen op uit te kunnen voeren
+            string nummerString = nummer.ToString();
+            //controleren of de string 13 tekens bevat
+            if (nummerString.Length != 13)
+                throw new ArgumentException();
+            //ints declareren om de tekens uit nummerString op te tellen, om het controlecijfer te bepalen
+            int even = 0, oneven = 0;
+            for (int x = 0; x < 12; ++x)
+            {
+                if (x % 2 == 0)
+                    oneven += int.Parse(nummerString[x].ToString());
+                else
+                    even += int.Parse(nummerString[x].ToString());
+            }
+            int som = oneven + 3 * even;
+            //controlecijfer bepalen
+            int controleCijfer = 0;
+            for (int x = 1; x <= 10; ++x)
+            {
+                if ((som + x) % 10 == 0)
+                {
+                    controleCijfer = (x == 10 ? 0 : x);
+                    break;
+                }
+            }
+            if (controleCijfer.ToString() == nummerString[12].ToString())
+                this.iSBNNummer = nummer;
+            else
+                throw new ArgumentException();
         }
         public override string ToString()
         {
-            throw new NotImplementedException();
+            return iSBNNummer.ToString();
         }
     }
 }
